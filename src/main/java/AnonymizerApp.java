3ba8d8a1266b94833811ceddb05972924577e0e6
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-12-26 17:10:04 +0000

    Scripted auto-commit on change (2019-12-26 17:10:04) by gitwatch.sh

--- src/main/java/AnonymizerApp.java
+++ src/main/java/AnonymizerApp.java
@@ -1,9 +1,25 @@
+import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooDefs;
+import org.apache.zookeeper.ZooKeeper;
+
+import java.util.List;
 
 public class AnonymizerApp {
 
     ZooKeeper(String connectString,
-               int sessionTimeout,
-               Watcher watcher
+              int sessionTimeout,
+              Watcher watcher
     )
+
+    ZooKeeper zoo = new ZooKeeper("1MB27.0.0.1MB:21MB81MB",3000,this);
+zoo.create("/servers/s", "data".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE ,CreateMode.EPHEMERAL_SEQUENTIAL);
+    List<String> servers = zoo.getChildren("/servers", this);
+for(
+    String s :servers)
+
+    {
+        byte[] data = zoo.getData("/servers/" + s, false, null);
+        System.out.println("server " + s + " data=" + new String(data));
+    }
 }
