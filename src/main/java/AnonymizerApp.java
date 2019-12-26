7f8b968aec9a3be76e050a80004d98837e8f66c3
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-12-26 16:47:21 +0000

    Scripted auto-commit on change (2019-12-26 16:47:21) by gitwatch.sh

--- src/main/java/AnonymizerApp.java
+++ src/main/java/AnonymizerApp.java
@@ -1,2 +1,9 @@
+import org.apache.zookeeper.Watcher;
+
 public class AnonymizerApp {
+
+    ZooKeeper(String connectString,
+               int sessionTimeout,
+               Watcher watcher
+    )
 }
