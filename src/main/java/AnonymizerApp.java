373408207f6911ddacb6d69d2f10e1aa3a1b8925
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-12-28 08:45:32 +0000

    Scripted auto-commit on change (2019-12-28 08:45:32) by gitwatch.sh

--- src/main/java/AnonymizerApp.java
+++ src/main/java/AnonymizerApp.java
@@ -32,12 +32,9 @@ public class AnonymizerApp {
 
         int port;
         String host = "localhost";
-        if (args.length > 0) {
-            host = args[0];
-        }
 
-        if (args.length > 1) {
-            port = Integer.parseInt(args[1]);
+        if (args.length > 0) {
+            port = Integer.parseInt(args[0]);
         } else {
             System.err.println("error");
             return;
