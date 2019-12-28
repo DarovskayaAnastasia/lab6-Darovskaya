4432e4da8c42fd82db1823f1029040654e17c633
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-12-28 07:50:44 +0000

    Scripted auto-commit on change (2019-12-28 07:50:44) by gitwatch.sh

--- src/main/java/AnonymizerApp.java
+++ src/main/java/AnonymizerApp.java
@@ -25,9 +25,20 @@ import java.util.concurrent.CompletionStage;
 import static akka.actor.TypedActor.context;
 
 public class AnonymizerApp {
-int port = 
 
     public static void main(String[] args) throws IOException, KeeperException, InterruptedException {
+
+        System.out.println("start!");
+
+        int port = 8001;
+        String host = "localhost";
+        if(args.length > 0){
+            host = args[0];
+        }
+        if (args.length > 1){
+            port = Integer.parseInt(args[1]);
+        }
+
 
         ActorSystem system = ActorSystem.create("routes"/*, ActorSystemSetup.empty()*/);
 
