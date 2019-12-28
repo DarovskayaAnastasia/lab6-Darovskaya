e8b3c37f15b9f46ad8b0627c41be4bef24b90262
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-12-28 01:40:07 +0000

    Scripted auto-commit on change (2019-12-28 01:40:07) by gitwatch.sh

--- src/main/java/ConfigurationKeeperActor.java
+++ src/main/java/ConfigurationKeeperActor.java
@@ -1,12 +1,18 @@
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
 
+import java.util.Random;
+
 public class ConfigurationKeeperActor extends AbstractActor {
 
-
+    private String[] servers;
 
     @Override
-    public Receive createRreceive() {
-        return ReceiveBuilder.create().match().build();
+    public Receive createReceive() {
+        return ReceiveBuilder.create().match(ServerListMessage.class, msg -> {
+            servers = msg.getServerList();
+        }).
+                match(RandomServerRequestMessage.class, msg -> sender().tell(new Random().nextInt(servers.length), self())
+                ).build();
     }
 }
