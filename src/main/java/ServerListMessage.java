b4deefe88537926af781c4487019b1ad34cc336c
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-12-28 01:31:25 +0000

    Scripted auto-commit on change (2019-12-28 01:31:25) by gitwatch.sh

--- src/main/java/ServerListMessage.java
+++ src/main/java/ServerListMessage.java
@@ -1,2 +1,10 @@
 public class ServerListMessage {
+
+    private final String[] serverList;
+
+    public ServerListMessage() {}
+
+    public ServerListMessage(String[] serverList) {
+        this.serverList = serverList;
+    }
 }
