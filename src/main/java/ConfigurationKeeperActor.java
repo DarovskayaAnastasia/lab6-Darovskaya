c9a5dd9c97437a76435dc37f79d3637aa4361737
Darovskaya Anastasia <stiltskenx@gmail.com>
2019-12-28 01:25:41 +0000

    Scripted auto-commit on change (2019-12-28 01:25:41) by gitwatch.sh

--- src/main/java/ConfigurationKeeperActor.java
+++ src/main/java/ConfigurationKeeperActor.java
@@ -1,2 +1,12 @@
-public class ConfigurationKeeperActor {
+import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
+
+public class ConfigurationKeeperActor extends AbstractActor {
+
+
+
+    @Override
+    public Receive createRreceive() {
+        return ReceiveBuilder.create().match().build();
+    }
 }
